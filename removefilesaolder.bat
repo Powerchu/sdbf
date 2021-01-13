82bee5ecb18404bdb438c871b6fdbb874dec59b4
Aaron Chu <m.chu@digipen.edu>
2021-01-13 19:15:44 +0800

    updated the .bat
    - fixed directory differences when opening with admin privileges and without
    - removed unnecessary echos and pauses

--- removefilesaolder.bat
+++ removefilesaolder.bat
@@ -4,6 +4,7 @@ echo [101;93m==================================================================
 echo [101;93m                 Scheduled Deletion Batch File v0.1                 [0m
 echo [101;93m====================================================================[0m
 echo [91mSDBF v0.1 is created by Aaron Chu, 13/1/2021.[0m
+echo.
 echo SDBF automatically adds this batch file to Microsoft Task Scheduler and runs it
 echo during windows logon. YOU ONLY NEED TO RUN THIS BATCH FILE MANUALLY ONCE.
 echo.
@@ -19,15 +20,6 @@ echo.
     
     set currDir=%~dp0
     set fileName=%~dpnx0
-	
-:: set Task Scheduler Bool
-    IF DEFINED SDBF_RUN_ONCE (
-        ECHO SDBF_RUN_ONCE IS defined
-    ) ELSE (
-    setx SDBF_RUN_ONCE 1
-        schtasks /Create /TR %fileName% /TN ScheduledDeletionTask /SC ONLOGON
-    )
-    pause
     
  :: set folder path 
 
@@ -37,10 +29,10 @@ echo.
     set biannualdump=%currDir%3_Bi-Annually
     
  :: create folders if do not exist
-    if not exist %dailydump%\NUL mkdir 0_Daily
-    if not exist %weeklydump%\NUL mkdir 1_Weekly
-    if not exist %monthlydump%\NUL mkdir 2_Monthly
-    if not exist %biannualdump%\NUL mkdir 3_Bi-Annually
+    if not exist %dailydump%\NUL mkdir %currDir%0_Daily
+    if not exist %weeklydump%\NUL mkdir %currDir%1_Weekly
+    if not exist %monthlydump%\NUL mkdir %currDir%2_Monthly
+    if not exist %biannualdump%\NUL mkdir %currDir%3_Bi-Annually
     
  :: set min age of files and folders to delete 
 
@@ -56,6 +48,13 @@ echo.
     set errName=deletelog.err
     
 :: First Time Use
+
+    :: Set Task Scheduler Bool
+    IF NOT DEFINED SDBF_RUN_ONCE (
+        setx SDBF_RUN_ONCE 1
+        schtasks /Create /TR %fileName% /TN ScheduledDeletionTask /SC ONLOGON
+    )
+
     IF NOT EXIST %logName% (
           echo ==================================================
           echo         Scheduled Deletion Batch File v0.1        
@@ -63,7 +62,7 @@ echo.
           echo.
           echo This file logs all deleted files as well as the date it was deleted.
           echo.
-        ) > %logName%
+        ) > %currDir%%logName%
         
     if not exist "%currDir%%errName%" (
           echo ==================================================
@@ -72,11 +71,12 @@ echo.
           echo.
           echo This file logs any errors prompted by forfiles.exe.
           echo.
-        ) > %errName%
+        ) > %currDir%%errName%
     
     
 :: Timer
     :loop
+	echo.
     choice /t 5 /c yn /d n /n /m "Do you want to remove the task? Deletion will continue after 5 seconds. (Y/N)?"
     if errorlevel 3 goto :loop
     if errorlevel 2 goto :no
@@ -126,8 +126,8 @@ echo.
     
     exit
     :change_setting
-	SchTasks /Delete /TN ScheduledDeletionTask /f
-	REG delete HKCU\Environment /F /V SDBF_RUN_ONCE
-	REG delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V SDBF_RUN_ONCE
-	
-	pause
+    SchTasks /Delete /TN ScheduledDeletionTask /f
+    REG delete HKCU\Environment /F /V SDBF_RUN_ONCE
+    REG delete "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /F /V SDBF_RUN_ONCE
+    
+    exit
